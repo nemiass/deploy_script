2db6e67f4ad178430f4dfa6cf9bca6d8cf554d4d
nemiass <nehemiastf@gmail.com>
2024-06-12 18:08:24 -0500

    add: extra commands

--- deploy_script.py
+++ deploy_script.py
@@ -26,13 +26,50 @@ def search_jar(path):
                 return os.path.join(root, file)
     return None
 
-def get_file_name_compress(name, module):
+def get_name_for_compress_file(project_name, module):
     now = datetime.now().strftime("%d-%m-%Y")
     module_name = "be" if module == "back" else "fe"
-    return f"{module_name}_{name}_{now}.tar.gz"
+    return f"{module_name}_{project_name}_{now}.tar.gz"
 
+def pre_git_command_process(project_dict):
+    post_compilation_commands = []
+    project_path = project_dict["path"]
+    extra_commands = project_dict.get("extra_commands", None)
 
-def compilation_project(project, module, server):
+    if not extra_commands or not isinstance(extra_commands, dict):
+        return post_compilation_commands
+
+    pre_compilation_commands = extra_commands.get("pre_compilation", None)
+    if not pre_compilation_commands or not isinstance(pre_compilation_commands, list):
+        return post_compilation_commands
+
+    post_compilation_commands = extra_commands.get("post_compilation", [])
+
+    options = ["y", "n"]
+
+    cprint("Proceso extra comandos", "yellow")
+    cprint(f"Comandos a ejecutar: {"->".join(pre_compilation_commands)} -> finalmente se ejecutará 'los comandos post' una vez que se haya terminado de compilar el proyecto", "yellow")
+    input_option = input("¿Ejecutar comandos? y/n: ").strip()
+    while input_option not in options:
+        cprint("Opción no válida", "red")
+        input_option = input("¿Ejecutar comandos? y/n: ").strip()
+    
+    if input_option == "n":
+        cprint("Proceso omitido", "yellow")
+    elif input_option == "y":
+        cprint(f"Inciando proceso en: {project_path}", "yellow")
+        for command in pre_compilation_commands:
+            cprint(f"Ejecutando comando: {command}", "yellow")
+            res = subprocess.run(command, cwd=project_path, shell=True)
+            time.sleep(3)
+            if res.returncode != 0:
+                cprint(f"Error en el comando {command}", "red")
+                exit(1)
+        cprint("Proceso finalizado, luego de la compilación se ejeutará comandos post compilación si están definidas", "green")
+
+    return post_compilation_commands
+
+def start_compilation_project(project, module, server):
     cprint(f"Compilando proyecto {project['name']} {module=}...", "yellow")
     if module == "back":
         commands = ["mvn", "clean", "package"]
@@ -48,7 +85,7 @@ def compilation_project(project, module, server):
         cprint("Proyecto compilado correctamente", "green")
 
 
-def compress_project(project_data, module):
+def compress_compiled_project(project_data, module):
     cprint(f"Comprimiendo proyecto {project_data['name']} {module=}...", "yellow")
     source_path = ""
     project_name = project_data["name"]
@@ -56,17 +93,18 @@ def compress_project(project_data, module):
     now = datetime.now().strftime("%d-%m-%Y")
     if module == "back":
         path_jar_file = search_jar(path_compilation)
-        if not path_jar_file:
+        if path_jar_file is None:
             cprint(f"No se encontró el archivo jar en {path_compilation}", "red")
             exit(1)
         new_name = f"{project_name}_{now}.jar"
+        cprint(f"Renombrando archivo jar {os.path.basename(path_jar_file)} a {new_name}...", "yellow")
         new_path = os.path.join(os.path.dirname(path_jar_file), new_name)
         os.rename(path_jar_file, new_path)
         source_path = new_path
     elif module == "front":
         source_path = path_compilation
 
-    compressed_file_name = get_file_name_compress(project_name, module)
+    compressed_file_name = get_name_for_compress_file(project_name, module)
     with tarfile.open(f"{PATH_SCRIPT}/{compressed_file_name}", "w:gz") as tar:
         if os.path.isdir(source_path):
             for root, _, files in os.walk(source_path):
@@ -78,9 +116,7 @@ def compress_project(project_data, module):
     cprint(f"Proyecto comprimido con éxito: {compressed_file_name}", "green")
     return compressed_file_name
 
-
-
-def upload_compress_file(file_name_compress, project, module, credentials):
+def upload_compress_file_to_server(file_name_compress, project, module, credentials):
     #config = Config(overrides={"sudo": {"password": credentials["password"]}})
     host = credentials["ip"]
     user = credentials["user"]
@@ -158,14 +194,34 @@ def main():
     os.environ["JAVA_HOME"] = config["JAVA_HOME"]
     project_data = config["PROJECTS"][project_name][module]
 
-    file_name = get_file_name_compress(project_data["name"], module)
+    file_name = get_name_for_compress_file(project_data["name"], module)
 
-    if not os.path.exists(f"{PATH_SCRIPT}/{file_name}"):
-        compilation_project(project_data, module, server)
-        file_name = compress_project(project_data, module)
+    option = None
+    if os.path.exists(f"{PATH_SCRIPT}/{file_name}"):
+        cprint(f"Archivo comprimido encontrado: {file_name}", "green")
+        option = input("¿Recompilar nuevamente? y/n: ").strip()
+    
+    if option is not None and option not in ["y", "n"]:
+        cprint("Opción no válida", "red")
+        exit(1)
+
+    if option is None or option == "y":
+        post_compilation_commands = pre_git_command_process(project_data)
+        start_compilation_project(project_data, module, server)
+
+        if len(post_compilation_commands) > 0:
+            for final_command in post_compilation_commands:
+                cprint(f"Ejecutando {final_command}", "yellow")
+                res = subprocess.run(final_command, cwd=project_data["path"], shell=True)
+                time.sleep(2)
+                if res.returncode != 0:
+                    cprint(f"Error en el proceso de '{final_command}'", "red")
+                    exit(1)
+
+        file_name = compress_compiled_project(project_data, module)
 
     crendentials = config["SSH_CREDENTIALS"][server]
-    upload_compress_file(file_name, project_data, module, crendentials)
+    upload_compress_file_to_server(file_name, project_data, module, crendentials)
 
 if __name__ == "__main__":
     try:
